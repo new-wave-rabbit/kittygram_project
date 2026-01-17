a844fff79f35bc1155eedf36126e5588bdefcac7
idotsenko <idotsenko@usergate.com>
2026-01-17 07:41:13 +0300

    Добавлена модель Owner

--- cats/models.py
+++ cats/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
